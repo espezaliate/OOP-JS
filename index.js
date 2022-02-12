0642373325b4de21c99db99ad3d7ba3453def14b
espezaliate <mkoziol54@gmail.com>
2022-02-12 13:43:31 +0100

    constructor and factory function

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log(draw);
-  },
-};
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1,
+//   },
+//   draw: function () {
+//     console.log(draw);
+//   },
+// };
 
-circle.draw();
+// circle.draw();
+
+// Factory Function - returns an object
+
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
+const circle = createCircle(1);
+
+// Constructor Function - when used this keyword and new
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const another = new Circle(1);
+
+// Creates a new object, sets this to object and returns object with function
